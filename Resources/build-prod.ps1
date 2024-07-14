31af45435cac0ae22b1f68281f4e39fd0e10a2c5
Hazel K <acomputerdog@gmail.com>
2024-07-14 15:23:00 -0400

    chore: create build script (resolves #28 and #29)

---
+++ Resources/build-prod.ps1
@@ -0,0 +1,40 @@
+﻿# [Production Build Script]
+# This is a placeholder until proper CI/CD pipelines are built.
+
+# Requirements:
+# * Powershell
+# * .NET SDK
+# * EF Core Tools
+# * Run from the solution root (not this folder)
+
+# Notes:
+# * https://learn.microsoft.com/en-us/dotnet/core/deploying/deploy-with-cli
+# * https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/compress-archive?view=powershell-7.4
+# * https://stackoverflow.com/questions/8095638/how-do-i-negate-a-condition-in-powershell
+# * https://stackoverflow.com/a/71159216
+
+$ReleaseVersion = '1.0.0-snapshot.0'
+$ReleaseDir = './Release'
+$PublishDir = './Publish'
+$BuildConfig = 'Release'
+$BuildProject = 'ModShark'
+
+# Clean publish directory
+if (Test-Path $PublishDir) {
+    rm -Recurse $PublishDir
+    mkdir $PublishDir
+}
+
+# Create release directory
+if (-Not (Test-Path $ReleaseDir)) {
+    mkdir $ReleaseDir
+}
+
+# Publish build
+dotnet clean $BuildProject --configuration $BuildConfig
+dotnet publish $BuildProject --configuration $BuildConfig --output $PublishDir
+dotnet ef migrations script --idempotent --project SharkeyDB --startup-project $BuildProject --output "$PublishDir/ModShark-migrations.sql"
+
+# Package build
+# Intentionally do *not* -Force in case someone forgets to update the release version.
+Compress-Archive -Path "$PublishDir/*" -DestinationPath "$ReleaseDir/ModShark-$ReleaseVersion.zip"
