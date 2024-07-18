85e3b55065acca4e51c364ad5f7e41797fd0a735
Hazel K <acomputerdog@gmail.com>
2024-07-18 14:19:17 -0400

    chore: update dev scripts

--- Resources/dev-scripts.sql
+++ Resources/dev-scripts.sql
@@ -1,18 +1,26 @@
-DELETE FROM ms_flagged_user
-;
-DELETE FROM ms_flagged_instance
-;
 DELETE FROM "abuse_user_report"
 WHERE comment LIKE 'ModShark: %'
+;
+DELETE FROM ms_flagged_user WHERE true
 ;
 INSERT INTO ms_queued_user (user_id)
 SELECT id as user_id
 FROM "user"
 ON CONFLICT DO NOTHING
 ;
+DELETE FROM ms_flagged_instance WHERE true
+;
 INSERT INTO ms_queued_instance (instance_id)
 SELECT id as instance_id
 FROM "instance"
+ON CONFLICT DO NOTHING
+;
+DELETE FROM ms_flagged_note WHERE true
+;
+INSERT INTO ms_queued_note (note_id)
+SELECT id as note_id
+FROM "note"
+LIMIT 50000
 ON CONFLICT DO NOTHING
 ;
 
