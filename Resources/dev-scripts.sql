0d3300d2fd31e7d9af07b0102508576b94d678a2
Hazel K <acomputerdog@gmail.com>
2024-07-20 23:56:17 -0400

    docs: add comments to dev-scripts.sql

--- Resources/dev-scripts.sql
+++ Resources/dev-scripts.sql
@@ -1,6 +1,9 @@
+-- Delete ModShark's native reports
 DELETE FROM "abuse_user_report"
 WHERE comment LIKE 'ModShark: %'
 ;
+
+-- Un-flag and re-queue all users 
 DELETE FROM ms_flagged_user WHERE true
 ;
 INSERT INTO ms_queued_user (user_id)
@@ -8,6 +11,8 @@ SELECT id as user_id
 FROM "user"
 ON CONFLICT DO NOTHING
 ;
+
+-- Un-flag and re-queue all instances
 DELETE FROM ms_flagged_instance WHERE true
 ;
 INSERT INTO ms_queued_instance (instance_id)
@@ -15,6 +20,8 @@ SELECT id as instance_id
 FROM "instance"
 ON CONFLICT DO NOTHING
 ;
+
+-- Un-flag and re-queue all notes
 DELETE FROM ms_flagged_note WHERE true
 ;
 INSERT INTO ms_queued_note (note_id)
@@ -24,9 +31,13 @@ LIMIT 50000
 ON CONFLICT DO NOTHING
 ;
 
-
+-- List all native reports
 SELECT * FROM "abuse_user_report";
 
+-- Count all scannable objects in the database
 SELECT COUNT(*) FROM "user"
 UNION ALL
-SELECT COUNT(*) FROM "instance";
+SELECT COUNT(*) FROM "instance"
+UNION ALL
+SELECT COUNT(*) FROM "note"
+;
