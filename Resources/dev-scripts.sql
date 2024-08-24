2270098ef42bd7de1a2b06945fea85ef7823da19
Hazel K <acomputerdog@gmail.com>
2024-08-24 15:01:34 -0400

    docs: add optional limits to dev-scripts.sql

--- Resources/dev-scripts.sql
+++ Resources/dev-scripts.sql
@@ -9,6 +9,7 @@ DELETE FROM ms_flagged_user WHERE true
 INSERT INTO ms_queued_user (user_id)
 SELECT id as user_id
 FROM "user"
+-- LIMIT 5000
 ON CONFLICT DO NOTHING
 ;
 
@@ -18,6 +19,7 @@ DELETE FROM ms_flagged_instance WHERE true
 INSERT INTO ms_queued_instance (instance_id)
 SELECT id as instance_id
 FROM "instance"
+-- LIMIT 20000
 ON CONFLICT DO NOTHING
 ;
 
@@ -27,7 +29,7 @@ DELETE FROM ms_flagged_note WHERE true
 INSERT INTO ms_queued_note (note_id)
 SELECT id as note_id
 FROM "note"
-LIMIT 50000
+-- LIMIT 50000
 ON CONFLICT DO NOTHING
 ;
 
