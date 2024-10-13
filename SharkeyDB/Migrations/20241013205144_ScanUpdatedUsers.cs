2ccb57ea147cf4c75e165fb63cf9b6f6cad1404a
Hazel K <acomputerdog@gmail.com>
2024-10-13 16:58:14 -0400

    re-scan notes after update

---
+++ SharkeyDB/Migrations/20241013205144_ScanUpdatedUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharkeyDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScanUpdatedUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                create trigger TRG_after_user_update
+                after update on "user"
+                for each row
+                execute function add_user_to_modshark_queue();
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop trigger TRG_after_user_update on \"user\";");
+        }
+    }
+}
